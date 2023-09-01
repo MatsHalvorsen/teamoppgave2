c6a896087c19240d3627284f14c52df2988b163c
MatsHalvorsen <82614331+MatsHalvorsen@users.noreply.github.com>
2023-09-01 09:00:28 +0200

    added a bananaPower function

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 //Model
+let isGameActive = true;
 let kaffometer = 4;
-const images = [ 
-    'https://www.toramo.games/img/battery0.png', 
-    'https://www.toramo.games/img/battery25.png', 
-    'https://www.toramo.games/img/battery50.png', 
-    'https://www.toramo.games/img/battery75.png', 
-    'https://www.toramo.games/img/battery100.png' ];
-// let bananometer = 25;
+let banana = 0;
+const images = [
+    'https://www.toramo.games/img/battery0.png',
+    'https://www.toramo.games/img/battery25.png',
+    'https://www.toramo.games/img/battery50.png',
+    'https://www.toramo.games/img/battery75.png',
+    'https://www.toramo.games/img/battery100.png'];
 const img2 = [
     'https://cdn.discordapp.com/attachments/1135484613872062464/1146725210725302352/Dead.png',
     'https://murphysmesshall.files.wordpress.com/2017/10/cf3fbb9b8c9baa8fafcccdc551545cf4-monday-face-garfield-pictures.jpg',
@@ -14,11 +15,18 @@ const img2 = [
     'https://cdn.discordapp.com/attachments/1135484613872062464/1146727055166296184/Give_more.png',
     'https://www.nicepng.com/png/detail/227-2271477_garfield-characters-logo-vector-happy-garfield.png',
 ];
+const img3 = [
+    'https://cdn.discordapp.com/attachments/1135484613872062464/1146782854299267182/bananAnsikt_transparent.png',
+];
+
+
+updateView();
+const coffeeInterval = setInterval(coffeeDrain, 1000);
+if (kaffometer < 0) {
+    clearInterval(coffeeInterval);
+}
 
 //View
-updateView();
-startCoffeeDrain();
-
 function updateView() {
     document.getElementById('app').innerHTML =  /*HTML*/ ` 
     <h1>NEED COFFEE</h1>
@@ -30,86 +38,54 @@ function updateView() {
         <div id="kaffeContainer">
             <img id="kaffe" src=" ${images[4]} ">
         </div>
+        <div id="bananaContainer">
+            <img id="kaffe" src=" ${img3[0]} ">
+        </div>
     </div>
     <button onclick="kaffeRefill()">Give coffee</button>
+    <button onclick="bananPower()">Need banana NOW!</button>
     
     `;
 }
 
 
 //Controller
-// if (kaffometer = 25)
-// return document.getElementById('kaffe').innerHTML = img
-// 
-// index = 1;
 
-// Tor var her (forslag til gradvis minking av kaffometer):
-// setInterval(function(){
-//     kaffometer--;
-//     if (kaffometer <= 0) {
-//         kaffometer = 0; // alert("Game over");
-//     }
-// }, 10000); // 10 sek
-
- //showKaffometer(0);
- function showKaffometer(index) {
-    if (index >=0 && index <=4) {
+function showKaffometer(index) {
+    if (index >= 0 && index <= 4) {
         document.getElementById('kaffeContainer').innerHTML = /*HTML*/`<img id="kaffe" src=" ${images[index]} ">`;
         document.getElementById('imgContainer').innerHTML = /*HTML*/`<img id="img1" src=" ${img2[index]} ">`;
+        document.getElementById('bananaContainer').innerHTML = /*HTML*/`<img id="img3" src=" ${img3[0]} ">`;
     }
-    // if (index == 0){
-    //     document.getElementById('kaffe') = "${img[0]}";
-    //  }
-    //  else if (index == 1){
-    //     document.getElementById('kaffe') = "${img[1]}";
-    //  }
-    //  else if (index == 2){
-    //     document.getElementById('kaffe') = "${img[2]}";
-    //  }
-    //  else if (index == 3){
-    //     document.getElementById('kaffe') ="${img[3]}";
-    //  }
-    //  else
-    //     document.getElementById('kaffe') = "${img[4]}";
-       
-     } 
+    }
 
-     function kaffeRefill() {
-    //    document.getElementById('kaffe').innerHTML = 
-    if (kaffometer >= 4) {
-        kaffometer = 4;
-    }
-    showKaffometer(kaffometer);
-    kaffometer++ ;
-     }
-     
-      // function coffeeDrain()
-      //  setTimeout();
-     //   const coffeeDrain = setInterval(function, 50000)
-        
-    function coffeeDrain() {
+function bananPower(){
+{
+    kaffometer = 4;
+}
+const coffeeInterval = setInterval(coffeeDrain, 1000);  // Eternal life :)
+showKaffometer(kaffometer);
+ } 
+ 
+
+function kaffeRefill() {
+    if (kaffometer > 0) {
+        kaffometer++;
+        if (kaffometer >= 4) {
+            kaffometer = 4;
+        }
         showKaffometer(kaffometer);
-        kaffometer--;
-        console.log("Kaffometer: " + kaffometer);
-        if (kaffometer == 0) {
-            // dead pus
-         //   alert ("Game over. Dead pus");
-        }
     }
-    
-    function startCoffeeDrain() {
-    setInterval(coffeeDrain(), 1000);
-    updateView();
-    }
+}
 
-     /* 
-     function autoTrafficLight() {
-            color1();
-            setTimeout(color2, 1000);
-            setTimeout(color3, 2000);
-            setTimeout(autoTrafficLight, 3000)
-        }
-        autoTrafficLight(); 
-        */
-       
-        
+function coffeeDrain() {
+    kaffometer--;
+    updateView();
+    showKaffometer(kaffometer);
+    //console.log("Kaffometer: " + kaffometer);
+    if (kaffometer == 0) {
+        clearInterval(coffeeInterval);  // Stop the timer at "dead pus"
+        //   alert ("Game over. Dead pus");
+    }
+}
+
